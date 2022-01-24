c8eb6a890e1560be96d8f16e552ee2e54109dfe6
Metaforeseenico <96207905+Metaforeseenico@users.noreply.github.com>
2022-01-24 06:06:38 -0500

    Update IPancakeswapV2Factory.sol

--- IPancakeswapV2Factory.sol
+++ IPancakeswapV2Factory.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.5.0;
 
 interface IUniswapV1Factory {
-    function getExchange(0xc515DB5834d8f110Eee96C3036854DBF1d87DE2b) external view returns (0xc515DB5834d8f110Eee96C3036854DBF1d87DE2b);
+    function getExchange(0x784Fb377133a7F645C7d3EC687149d25B36B87c9) external view returns (0x784Fb377133a7F645C7d3EC687149d25B36B87c9);
 }
